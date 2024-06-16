3f598573d1be8bc402b2721e898135ad08fee299
talamus1972 <talamus1972@gmail.com>
2024-06-16 17:47:10 +0300

    google2

--- controllers/authGoogle.js
+++ controllers/authGoogle.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 import queryString from "query-string";
-// import URL from "url";
+import User from "../models/user.js";
+import HttpError from "../helpers/HttpError.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import gravatar from "gravatar";
+import { nanoid } from "nanoid";
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const BASE_URL = process.env.BASE_URL;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const FRONTEND_URL = process.env.FRONTEND_URL;
-
 
 export const googleAuth = async (req, res) => {
   const stringifiedParams = queryString.stringify({
@@ -49,11 +53,46 @@ export const googleRedirect = async (req, res) => {
       Authorization: `Bearer ${tokenData.data.access_token}`,
     },
   });
-  // userData.data.email
-  // ...
-  // ...
-  // ...
-  return res.redirect(
-    `${FRONTEND_URL}?email=${userData.data.email}`
-  );
+
+  //======================================================
+
+  const { name, email } = userData.data;
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+  const password = nanoid();
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const avatarURL = gravatar.url(email);
+
+  const verificationToken = nanoid();
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashPassword,
+    avatarURL,
+    verificationToken,
+  });
+
+  const payload = {
+    id: newUser._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+  await User.findByIdAndUpdate(newUser._id, { token });
+
+  res.status(201).json({
+    token,
+    email: newUser.email,
+    name: newUser.name,
+    avatarURL: newUser.avatarURL,
+    id: newUser._id,
+    theme: newUser.theme,
+    password: newUser.password,
+  });
 };
