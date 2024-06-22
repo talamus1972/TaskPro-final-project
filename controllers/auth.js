97d15c3b501260295dd47e58b600b5d55adb30f1
talamus1972 <talamus1972@gmail.com>
2024-06-22 20:52:29 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ import User from "../models/user.js";
 import HttpError from "../helpers/HttpError.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-// import gravatar from "gravatar";
+import gravatar from "gravatar";
 // import path from "node:path";
 
 import { nanoid } from "nanoid";
